64368327eb1d97bd52192d359259d64e2104669e
goni <gonina@gmail.com>
2019-05-26 15:35:53 +0300

    3 added

--- hw5.py
+++ hw5.py
@@ -1,7 +1,9 @@
 import json
-from typing import Union
+from typing import Union, Tuple
 import pathlib
 import pandas as pd
+import matplotlib.pyplot
+import numpy as np
 
 class QuestionnaireAnalysis:
     """
@@ -20,11 +22,41 @@ class QuestionnaireAnalysis:
         """
         self.data = pd.read_json(self.data_fname)
 
+    def show_age_distrib(self) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Calculates and plots the age distribution of the participants.
+        Returns a tuple containing two numpy arrays:
+        The first item being the number of people in a given bin.
+        The second item being the bin edges.
+        """
+        # groupby age
+    def plot_hist (self):
+        pass
+
+    def calc_dist (self):
+        pass
+
+        """Plot the distribution of ages of the participants. 
+        The bins for the histogram should be [0, 10), [10, 20), [20, 30),
+         ..., [90, 100). The function should return the result."""
+
+    def remove_rows_without_mail(self) -> pd.DataFrame:
+        """
+        Checks self.data for rows with invalid emails, and removes them.
+        Returns the corrected DataFrame, i.e. the same table but with
+        the erroneous rows removed and the (ordinal) index after a reset.
+        """
+        valid_email = self.data['email'].str.contains('@' and '.co')
+        df_valid_email = self.data.where(valid_email)
+        df_valid_email = df_valid_email.dropna(subset=['email'])
+        print (df_valid_email)
 
 if __name__ == "__main__":
     data_fname = 'data.json'
     d = QuestionnaireAnalysis(data_fname)
     d.read_data()
-    print (d.data)
+    # print (d.data)
+    d.remove_rows_without_mail()
+    
         
         
